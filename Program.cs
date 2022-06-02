b3adc97d444bafb5c9b26a192f573e8a879c1e71
EgeniiM <106548205+EgeniiM@users.noreply.github.com>
2022-06-02 22:35:24 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals;
+            int crystalPrise = 3;
+            string input;
+
+            Console.WriteLine("Здравствуйте!! Сколько у вас золота?");
+            input = Console.ReadLine();
+            gold = Convert.ToInt32(input);
+            Console.WriteLine("Кристал стоит 3 золотых, сколько хотите купить?");
+            input = Console.ReadLine();
+            crystals = Convert.ToInt32(input);
+            gold -= crystals * crystalPrise;
+            Console.WriteLine($"У вас {gold} золота и {crystals} кристаллов");
+        }
+    }
+}
